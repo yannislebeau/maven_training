59aab9e244b4cc77e2a3ac34d9e3ae5ff5fbfbf2
Yannis Lebeau <yannis77@icloud.com>
2021-11-30 12:34:38 +0100

    Add live badges

---
+++ README.md
@@ -0,0 +1,2 @@
+![example workflow](https://github.com/yannislebeau/maven_training/actions/workflows/build.yml/badge.svg)
+![test]( https://codecov.io/gh/yannislebeau/maven_training/settings/badge)
---
+++ src/test/java/fr/yannislebeau/SampleTest.java
@@ -0,0 +1,21 @@
+package fr.yannislebeau;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SampleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void op() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fact() {
+    }
+}
